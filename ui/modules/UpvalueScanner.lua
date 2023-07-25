16fa15a54064a8388d2a0b2eb3bffbf31301057a
nunera <67439647+nunera@users.noreply.github.com>
2023-07-25 09:43:35 +0000

    eeee

--- ui/modules/UpvalueScanner.lua
+++ ui/modules/UpvalueScanner.lua
@@ -559,7 +559,7 @@ end)
 
 viewElementsContext:SetCallback(function()
     print("sad")
-    warn(eahwhea)
+    warn("ewahawhawh")
     local temporaryElements = selectedUpvalue and selectedUpvalue.TemporaryElements
     local newHeight = 0
 
